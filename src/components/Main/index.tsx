49f63a88753c6b2186929882e363b5403758bddc
Eduardo-Vitesse <edu.dev.py@gmail.com>
2022-05-06 21:15:54 -0300

    add storybook in project

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 export function Main() {
   return (
-    <main>
-      <h2>React Avançado</h2>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Tittle>React Avançado</S.Tittle>
+      <S.Description>
+        Typescript, ReactJS Nextjs e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para a tela com codigo"
+      />
+    </S.Wrapper>
   )
 }
