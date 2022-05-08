4039544ed3f296bcd1fad067dc84ca838d3c2bb0
Eduardo-Vitesse <edu.dev.py@gmail.com>
2022-05-08 00:36:58 -0300

    tranform this project in a boilerplate on github

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,17 @@
 import * as S from './styles'
 
-export function Main() {
+export function Main({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS Nextjs e Styled Components'
+}) {
   return (
     <S.Wrapper>
       <S.Logo
         src="/img/logo.svg"
         alt="Imagem de um átomo e React Avançado escrito ao lado"
       />
-      <S.Tittle>React Avançado</S.Tittle>
-      <S.Description>
-        Typescript, ReactJS Nextjs e Styled Components
-      </S.Description>
+      <S.Tittle>{title}</S.Tittle>
+      <S.Description>{description}</S.Description>
       <S.Illustration
         src="/img/hero-illustration.svg"
         alt="Um desenvolvedor de frente para a tela com codigo"
